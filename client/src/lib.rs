aeb1820474ae2f4c7e44ca9265aaba1a219d2be9
tmvkrpxl0 <tmvkrpxl0@proton.me>
2026-01-10 23:06:32 +0900

    Client Window

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// crate::add
+// crate::state
+// crate::state::greet
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+use winit::event_loop::EventLoop;
+use crate::app::App;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+mod state;
+mod app;
+
+pub fn run() -> anyhow::Result<()> {
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
     }
+    #[cfg(target_arch = "wasm32")]
+    {
+        console_log::init_with_level(log::Level::Info).unwrap_throw();
+    }
+
+    let event_loop = EventLoop::with_user_event().build()?;
+    let mut app = App::new(
+        #[cfg(target_arch = "wasm32")]
+        &event_loop
+    );
+    event_loop.run_app(&mut app)?;
+
+    Ok(())
 }
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(start)]
+pub fn run_web() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+    run().unwrap_throw();
+
+    Ok(())
+}
+
